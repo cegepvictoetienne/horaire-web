3b5167199f18c143e3eb923cf8deb1c70a12262d
Etienne Rivard <etienne@kerzo.ca>
2024-10-17 13:05:24 -0400

    ajout de licence GPLv3

--- src/models/ientree.ts
+++ src/models/ientree.ts
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2024 Étienne Rivard
+ *
+ * Ce programme est un logiciel libre : vous pouvez le redistribuer et/ou
+ * le modifier selon les termes de la Licence Publique Générale GNU, telle
+ * que publiée par la Free Software Foundation, soit la version 3 de
+ * la licence, soit (à votre choix) toute version ultérieure.
+ *
+ * Ce programme est distribué dans l'espoir qu'il sera utile,
+ * mais SANS AUCUNE GARANTIE ; sans même la garantie implicite de
+ * COMMERCIALISABILITÉ ou d'ADÉQUATION À UN OBJECTIF PARTICULIER.
+ * Voir la Licence Publique Générale GNU pour plus de détails.
+ *
+ * Vous devriez avoir reçu une copie de la Licence Publique Générale GNU
+ * avec ce programme. Si ce n'est pas le cas, consultez
+ * <https://www.gnu.org/licenses/>.
+ */
+
 export default interface IEntree {
   nomCours: string;
   salle: string;
